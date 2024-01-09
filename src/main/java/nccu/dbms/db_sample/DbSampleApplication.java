d8bee9ec3a5ecf162ca69ab2cf25151fe3ce2f36
Andy0223 <andy.lee@bluextrade.com>
2024-01-09 22:46:02 +0800

    spring boot project init for JDK 17

---
+++ src/main/java/nccu/dbms/db_sample/DbSampleApplication.java
@@ -0,0 +1,13 @@
+package nccu.dbms.db_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSampleApplication.class, args);
+	}
+
+}
